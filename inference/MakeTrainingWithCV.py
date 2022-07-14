18b760f52f266a0f9c35d3f365709ea5962a9652
JoshBrafman <jmbrafma@mail.yu.edu>
2022-07-14 01:16:03 +0000

    added code to calculate iou

--- inference/MakeTrainingWithCV.py
+++ inference/MakeTrainingWithCV.py
@@ -20,14 +20,13 @@ import random
 
 keys = ['cat','dog','horse','sheep','cow','elephant','bear','zebra','giraffe']
 numbers = [15,16,17,18,19,20,21,22,23]
-coordinates = []
 
 cv2.setUseOptimized(True)
 cv2.setNumThreads(4)
 
 coco_data = tfds.load('coco', split='train', shuffle_files=True)
 for num,example in enumerate(coco_data):
-   if num > 20:
+   if num > 5:
     break #just want to test it on small amount first
    image = example['image']
    labels = example['objects']['label']
@@ -37,33 +36,58 @@ for num,example in enumerate(coco_data):
    numpyImage=Image.fromarray(image.numpy())
    im = image.numpy()
    ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
-   ss.setBaseImage(numpyImage)
+   ss.setBaseImage(im)
    ss.switchToSelectiveSearchQuality()
    rects = ss.process()
    for count,label in enumerate(labelNums):
     if label in numbers:
+        print("YAY")
         bbox = bboxes[count].numpy()
-        scores = []
         print('Total Number of Region Proposals: {}'.format(len(rects)))
-        numShowRects = 5000
+        numShowRects = 4000
+        scores = []
+        coordinates = []
         for i, rect in enumerate(rects):
             if (i < numShowRects):
                 x, y, w, h = rect
-                top, left, bottom, right = bbox[0].numpy(), bbox[1].numpy(), bbox[2].numpy(), bbox[3].numpy() #this is a tensorflow box of an animal
+                top, left, bottom, right = bbox[0], bbox[1], bbox[2], bbox[3] #this is a tensorflow box of an animal
                 top2, left2, bottom2, right2 = float(y), float(x), float(y+h), float(x+w) #this is a box from opencv
-                score = #the iou between the two boxes
-                scores.append(score)
+                coordinates.append([float(y), float(x), float(y+h), float(x+w)])
+                xA = max(left, left2)
+                yA = max(top, top2)
+                xB = min(right,right2)
+                yB = min(bottom, bottom2)
+                # compute the area of intersection rectangle
+                interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+                # compute the area of both the prediction and ground-truth
+                # rectangles
+                boxAArea = (right - left + 1) * (bottom - top + 1)
+                boxBArea = (right2 - left2 + 1) * (bottom2 - top2 + 1)
+                # compute the intersection over union by taking the intersection
+                # area and dividing it by the sum of prediction + ground-truth
+                # areas - the interesection area
+                iou = interArea / float(boxAArea + boxBArea - interArea)
+                scores.append(float(iou))
             else:
                 break
+        print('working on thing now')
         scores = tf.convert_to_tensor(scores)
+        coordinates = tf.convert_to_tensor(coordinates)
         max_output_size = 2 #how many more boxes do we want per animal? Probably just like 2
-        iou_threshold = 0.3 #this is NOT the iou from above, it's what iou we want to be allowed between the 2 (or more) new boxes we make. What should it be?
+        iou_threshold = .9 #this is NOT the iou from above, it's what iou we want to be allowed between the 2 (or more) new boxes we make. What should it be?
         max_output_size = tf.convert_to_tensor(max_output_size)
         iou_threshold = tf.convert_to_tensor(iou_threshold)
-        selected_indices = tf.image.non_max_suppression(coordinates, scores, max_output_size, iou_threshold, score_threshold = .7) #how strongly do we want our new boxes to overlap with the original tensorflow animal box
+        selected_indices = tf.image.non_max_suppression(coordinates, scores, max_output_size, iou_threshold, score_threshold = .4) #how strongly do we want our new boxes to overlap with the original tensorflow animal box
         selected_boxes = tf.gather(coordinates, selected_indices)
-        #take code from other things to cut an image from the tensorflow box (unless we're running this code as an add-on, in which case that's already been done)
-        #and cut images from the extra boxes too this time and save them all to the class training data
+        print(selected_boxes)
+        copy = im.copy()
+        copy = Image.fromarray(copy)
+        for index, box in enumerate(selected_boxes):
+            box = box.numpy()
+            top, left, bottom, right = box[0], box[1], box[2], box[3]
+            draw = ImageDraw.Draw(copy)
+            draw.rectangle([left, top, right, bottom], outline = "red")
+            copy.save("newcroptesting/" +count +index +".jpg")
 
 
 
