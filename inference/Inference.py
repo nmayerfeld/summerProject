39f2c858587425b375e321ba239836ded1deda1a
sturma34 <akivasturm@gmail.com>
2022-07-25 09:23:57 +0000

    new inference

--- inference/Inference.py
+++ inference/Inference.py
@@ -4,6 +4,7 @@ import PIL
 from PIL import Image, ImageDraw, ImageFont
 import tensorflow as tf
 import pathlib
+import tensorflow_addons as tfa
 
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -16,98 +17,135 @@ from time import sleep
 cv2.setUseOptimized(True);
 cv2.setNumThreads(4);
 
-model = load_model('/home/ec2-user/visionaries/ProjectRepo/summerProject/model4.h5')
-class_names = ['background',  'bear', 'cat', 'cow', 'dog', 'elephant', 'giraffe', 'horse', 'sheep', 'teddy bear','zebra']
+model = load_model('/home/ubuntu/visionaries/summerProject/inference/RNm5PostTune.h5')
+class_names = ['background', 'bear', 'cat', 'cow', 'dog', 'elephant', 'giraffe', 'horse', 'sheep', 'zebra']
 
 img_height = 160
 img_width = 160
 
-coordinates = []
-dict = {}
+directory='/home/ubuntu/WholeImageTests/horse'
+for filename in os.listdir(directory):
+    coordinates = []
+    dict = {}
+    link = os.path.join(directory, filename)
+    im = cv2.imread(link)
+    imOut = im.copy()
 
-link = "/home/ec2-user/visionaries/WholeImageTests/cow/image26044crop0.jpg"
+    #coordinates = []
+    #dict = {}
 
-im = cv2.imread(link)
-imOut = im.copy()
+    #link = "/home/ubuntu/WholeImageTests/zebra/image41040crop0.jpg"
 
-# create Selective Search Segmentation Object using default parameters
-ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+    #im = cv2.imread(link)
+    #imOut = im.copy()
 
-# set input image on which we will run segmentation
-ss.setBaseImage(im)
+    # create Selective Search Segmentation Object using default parameters
+    ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
 
-ss.switchToSelectiveSearchQuality()
+    # set input image on which we will run segmentation
+    ss.setBaseImage(im)
 
-rects = ss.process()
-print('Total Number of Region Proposals: {}'.format(len(rects)))
+    ss.switchToSelectiveSearchQuality()
 
-# number of region proposals to show
-numShowRects = 1700
+    rects = ss.process()
+    print('Total Number of Region Proposals: {}'.format(len(rects)))
 
-# itereate over all the region proposals
-for i, rect in enumerate(rects):
-    x, y, w, h = rect
-    coordinates.append([float(y), float(x), float(y+h), float(x+w)])
+    # number of region proposals to show
+    numShowRects = 15000
 
-coordinates = tf.convert_to_tensor(coordinates)
-scores = []
-pics = []
-for box in coordinates:
-    copy = im.copy()
-    copy = Image.fromarray(copy)
-    box = box.numpy()
-    top, left, bottom, right = box[0], box[1], box[2], box[3]
-    croppedImage = copy.crop((left, top, right, bottom))
-    croppedImage = croppedImage.resize((img_height, img_width))
-    img_array = tf.keras.utils.img_to_array(croppedImage)
-    pics.append(img_array)
+    # itereate over all the region proposals
+    for i, rect in enumerate(rects):
+        x, y, w, h = rect
+        if w > 60 and h > 60:
+            coordinates.append([float(y), float(x), float(y+h), float(x+w)])
 
 
-np_pics = np.array(pics)
-print("making predictions")
-predictions = model.predict(np_pics)
-print("Predictions completed.")
-sleep(3)
+    coordinates = tf.convert_to_tensor(coordinates)
+    scores = []
+    pics = []
+    for box in coordinates:
+        copy = im.copy()
+        RGB_image = cv2.cvtColor(copy, cv2.COLOR_BGR2RGB)
+        copy = Image.fromarray(RGB_image)
+        box = box.numpy()
+        top, left, bottom, right = box[0], box[1], box[2], box[3]
+        croppedImage = copy.crop((left, top, right, bottom))
+        croppedImage = croppedImage.resize((img_height, img_width))
+        img_array = tf.keras.utils.img_to_array(croppedImage)
+        pics.append(img_array)
 
-for index, prediction in enumerate(predictions):
-    score = tf.nn.softmax(prediction)
-    #print("score: \n" + str(score))
-    #print("Score type: " + str(type(score)))
-    if (class_names[np.argmax(score)] == "background"):
-        scores.append(0)
-        #print("it's a background")
-    else:
-        scores.append(np.max(score))
-    print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
-    top, left, bottom, right =  coordinates[index][0].numpy(), coordinates[index][1].numpy(), coordinates[index][2].numpy(), coordinates[index][3].numpy(),
-    dict[(top, left, bottom, right)] = class_names[np.argmax(score)]
+    np_pics = np.array(pics)
+    print("making predictions")
+    predictions = model.predict(np_pics)
+    print("Predictions completed.")
+    sleep(3)
 
-scores = tf.convert_to_tensor(scores)
 
-max_output_size = 7
-iou_threshold = 0.01
+    for index, prediction in enumerate(predictions):
+        score = prediction
+        top, left, bottom, right =  coordinates[index][0].numpy(), coordinates[index][1].numpy(), coordinates[index][2].numpy(), coordinates[index][3].numpy(),
+        dict[(top, left, bottom, right)] = class_names[np.argmax(score)]
 
-max_output_size = tf.convert_to_tensor(max_output_size)
-iou_threshold = tf.convert_to_tensor(iou_threshold)
-
-selected_indices = tf.image.non_max_suppression(
-    coordinates, scores, max_output_size, iou_threshold, score_threshold = .7)
-
-selected_boxes = tf.gather(coordinates, selected_indices)
-im = Image.fromarray(im)
-
-for box in selected_boxes:
-    top, left, bottom, right = box[0].numpy(), box[1].numpy(), box[2].numpy(), box[3].numpy()
-    draw = ImageDraw.Draw(im)
-    draw.rectangle([left, top, right, bottom], outline = "red")
-    draw.text((left + 3, top + 3), dict[(top, left, bottom, right)], color = "red")
+        if (class_names[np.argmax(score)] == "background"):
+            scores.append(0)       
+        else:
+            scores.append(np.max(score))
+        print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+        )
         
-try:
-   os.mkdir("boxed_images")
-except OSError as error:
-   pass
 
-im.save("boxed_images/" + os.path.basename(link))
+
+    scores = tf.convert_to_tensor(scores)
+
+    max_output_size = 30
+    iou_threshold = 0.6
+
+    max_output_size = tf.convert_to_tensor(max_output_size)
+    iou_threshold = tf.convert_to_tensor(iou_threshold)
+
+    selected_indices = tf.image.non_max_suppression(
+        coordinates, scores, max_output_size, iou_threshold, score_threshold = .995)
+
+    selected_boxes = tf.gather(coordinates, selected_indices)
+
+    sizes = []
+    coordinates2 = []
+    for box in selected_boxes:
+        top, left, bottom, right = box[0].numpy(), box[1].numpy(), box[2].numpy(), box[3].numpy()
+        sizes.append((right - left)*(bottom-top))
+        coordinates2.append([float(top), float(left), float(bottom), float(right)])
+    sizes = tf.convert_to_tensor(sizes)
+    coordinates2 = tf.convert_to_tensor(coordinates2)
+
+    if len(coordinates2) != 0:
+        
+
+        max_output_size = 7
+        iou_threshold = 0.01
+
+        max_output_size = tf.convert_to_tensor(max_output_size)
+        iou_threshold = tf.convert_to_tensor(iou_threshold)
+
+        selected_indices = tf.image.non_max_suppression(
+            coordinates2, sizes, max_output_size, iou_threshold, score_threshold = .01)
+
+        selected_boxes = tf.gather(coordinates2, selected_indices)
+
+
+        imageRGB = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        im = Image.fromarray(imageRGB)
+
+        for box in selected_boxes:
+            top, left, bottom, right = box[0].numpy(), box[1].numpy(), box[2].numpy(), box[3].numpy()
+            draw = ImageDraw.Draw(im)
+            draw.rectangle([left, top, right-1, bottom-1], outline = "red")
+            draw.text((left + 3, top + 3), dict[(top, left, bottom, right)], color = "red")
+                
+        try:
+            os.mkdir("PostPatchInferencedImages")
+        except OSError as error:
+            pass
+
+        im.save("/home/ubuntu/PostPatchInferencedImages/" + os.path.basename(link))
